8460973a920da1c8437c4c23396a05e7257ff80f
njm3754 <32784910+njm3754@users.noreply.github.com>
2019-02-23 15:07:14 -0600

    Add files via upload

--- Warehouse/Supplies.java
+++ Warehouse/Supplies.java
@@ -1,4 +1,28 @@
 
 public class Supplies {
-
+	
+	private Manufacturer manufacturer; 
+	private Product product;
+	private float price; 
+	
+	private Supplies(Manufacturer manufactuerer, Product product, float price) {
+		this.manufacturer = manufactuerer;
+		this.product = product;
+		this.price = price;
+	}
+	
+	public Manufacturer getManufacturer() {
+		return manufacturer;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public float getPrice() {
+		return price;
+	}
+	
+	public String toString() {
+	    return  manufacturer.toString() +  product.toString() + " Price " + price;
+	  }
+	  
 }
